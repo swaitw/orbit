170a920603d4895e5b2570814435c0bad034be60
Matija Marohnić <matija.marohnic@gmail.com>
2020-11-04 12:46:45 +0100

    test(InputFile): rewrite to Testing Library

--- packages/orbit-components/src/InputFile/__tests__/index.test.js
+++ packages/orbit-components/src/InputFile/__tests__/index.test.js
@@ -1,108 +1,73 @@
 // @flow
 import * as React from "react";
-import { shallow } from "enzyme";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import InputFile from "../index";
 import SPACINGS_AFTER from "../../common/getSpacingToken/consts";
+import defaultTheme from "../../defaultTheme";
 
-describe(`InputFile with help`, () => {
-  const label = "Select file";
-  const buttonLabel = "Click on me";
-  const name = "name";
-  const placeholder = "Not file has been selected";
-  const fileName = "fileName.png";
-  const dataTest = "test";
-  const tabIndex = "-1";
-  const allowedFileTypes = [".png", ".jpg", ".pdf"];
-  const onChange = jest.fn();
-  const onFocus = jest.fn();
-  const onBlur = jest.fn();
-  const onRemoveFile = jest.fn();
-  const spaceAfter = SPACINGS_AFTER.NORMAL;
-
-  const component = shallow(
-    <InputFile
-      name={name}
-      label={label}
-      buttonLabel={buttonLabel}
-      placeholder={placeholder}
-      fileName={fileName}
-      dataTest={dataTest}
-      allowedFileTypes={allowedFileTypes}
-      spaceAfter={spaceAfter}
-      help={
-        <div>
-          Supported files: <strong>PNG, JPG, PDF</strong>
-        </div>
-      }
-      tabIndex={tabIndex}
-      onChange={onChange}
-      onFocus={onFocus}
-      onBlur={onBlur}
-      onRemoveFile={onRemoveFile}
-    />,
-  );
-  const input = component.find("InputFile__Input");
-  const field = component.find("InputFile__Field");
-  const closeButton = component.find("ButtonLink");
-
-  it("should contain a label", () => {
-    expect(component.find("FormLabel").render().text()).toBe(label);
+describe("InputFile", () => {
+  it("should have expected DOM output", () => {
+    const { container } = render(
+      <InputFile
+        name="name"
+        label="Label"
+        buttonLabel="Select file"
+        placeholder="No file selected"
+        dataTest="test"
+        allowedFileTypes={[".png", ".jpg", ".pdf"]}
+        spaceAfter={SPACINGS_AFTER.NORMAL}
+        help="help message"
+        tabIndex="-1"
+      />,
+    );
+    const input = screen.getByLabelText(/Label/);
+    expect(input).toHaveAttribute("type", "file");
+    expect(input).toHaveAttribute("name", "name");
+    expect(screen.getByRole("button", { name: "Select file" })).toBeInTheDocument();
+    expect(screen.getByText(/No file selected/)).toBeInTheDocument();
+    expect(screen.getByTestId("test")).toBeInTheDocument();
+    expect(input).toHaveAttribute("accept", ".png,.jpg,.pdf");
+    expect(container.firstChild).toHaveStyle({ marginBottom: defaultTheme.orbit.spaceSmall });
+    expect(screen.getByText("help message")).toBeInTheDocument();
+    expect(input).toHaveAttribute("tabindex", "-1");
+    expect(input).toHaveAttribute("data-state", "ok");
   });
-  it("should contain an input", () => {
-    expect(input.exists()).toBe(true);
+  it("should render given file name", () => {
+    render(<InputFile placeholder="No file selected" fileName="fileName.png" />);
+    expect(screen.getByText(/fileName\.png/)).toBeInTheDocument();
+    expect(screen.queryByText(/No file selected/)).not.toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "" })).toBeInTheDocument();
   });
-  it("should have passed props", () => {
-    expect(component.find("InputFile__Input").render().prop("attribs").name).toBe(name);
-    expect(input.prop("accept")).toBe(allowedFileTypes);
-    expect(field.prop("spaceAfter")).toBe(spaceAfter);
-    expect(input.render().prop("tabindex")).toBe(tabIndex);
-    expect(input.render().prop("data-test")).toBe(dataTest);
-    expect(input.render().prop("data-state")).toBe("ok");
+  it("should render given error", () => {
+    render(<InputFile label="Label" error="error message" />);
+    expect(screen.getByText("error message")).toBeInTheDocument();
+    expect(screen.getByLabelText(/Label/)).toHaveAttribute("data-state", "error");
   });
-  it("should contain a input Button", () => {
-    expect(component.find("InputFile__FakeInput").find("Button").exists()).toBe(true);
-  });
-  it("should contain a close Button", () => {
-    expect(closeButton.exists()).toBe(true);
-  });
-  it("should contain a fake div with styling", () => {
-    expect(component.find("InputFile__FakeInput").exists()).toBe(true);
-  });
-  it("should contain FeedBack", () => {
-    expect(component.find(`FormFeedback`).exists()).toBe(true);
-  });
-  it("should execute onChange method", () => {
-    input.simulate("change");
+  it("should trigger event handlers", () => {
+    const onFocus = jest.fn();
+    const onChange = jest.fn();
+    const onBlur = jest.fn();
+    const onRemoveFile = jest.fn();
+    render(
+      <InputFile
+        label="Label"
+        fileName="fileName.png"
+        onFocus={onFocus}
+        onChange={onChange}
+        onBlur={onBlur}
+        onRemoveFile={onRemoveFile}
+      />,
+    );
+    const input = screen.getByLabelText(/Label/);
+    const file = new File(["hello"], "hello.png", { type: "image/png" });
+    userEvent.upload(input, file);
+    expect(onFocus).toHaveBeenCalled();
     expect(onChange).toHaveBeenCalled();
-  });
-  // we can not check if onFocus has been called on input[type="file"]
-  // we also need to convert it
-  it("should have onFocus method", () => {
-    expect(input.prop("onFocus").toString()).toBe(onFocus.toString());
-  });
-  it("should execute onBlur method", () => {
-    input.simulate("blur");
     expect(onBlur).toHaveBeenCalled();
-  });
-  it("should have onRemoveFile method", () => {
-    closeButton.simulate("click", { preventDefault() {} });
+    const closeButton = screen.getByRole("button", { name: "" });
+    userEvent.click(closeButton);
     expect(onRemoveFile).toHaveBeenCalled();
-  });
-});
-describe(`InputFiInputFile with error`, () => {
-  const component = shallow(
-    <InputFile error={<div>Error message (explain how to solve it)</div>} />,
-  );
-
-  it("should NOT contain a label", () => {
-    expect(component.find("FormLabel").exists()).toBe(false);
-  });
-  it("should contain FeedBack", () => {
-    expect(component.find(`FormFeedback`).exists()).toBe(true);
-  });
-
-  it("should has data-state error", () => {
-    expect(component.find("InputFile__Input").render().prop("data-state")).toBe("error");
   });
 });
